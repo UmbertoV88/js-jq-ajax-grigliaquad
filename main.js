d8640d6c716773957fc9f2e06e466bff3cbaad61
UmbertoV <umberto.ottone@gmail.com>
2019-12-13 09:06:55 +0100

    Pulito il codice con una funzione esterna

--- main.js
+++ main.js
@@ -4,33 +4,35 @@ $(document).ready(function() {
 
     }
     $(".quadrato").click(function(){
-        var quadCorrente = $(this);
-        $.ajax({
-            url : "https://flynn.boolean.careers/exercises/api/random/int",
-            method : "GET",
-            success : function (data){
-                if (!quadCorrente.hasClass("yellow") || !quadCorrente.hasClass("green")) {
-                    if (data.response <= 5) {
-                        quadCorrente.addClass("yellow");
-                        console.log(data.response);
-                    }else{
-                        quadCorrente.addClass("green");
-                        console.log(data.response);
-                    }
-                }
-            },
-            error : function(){
+        if (!($(this).hasClass("yellow") || $(this).hasClass("green"))) {
+            var quadCorrente = $(this);
+            $.ajax({
+                url : "https://flynn.boolean.careers/exercises/api/random/int",
+                method : "GET",
+                success : function (data){
+                    var numeroRandom = data.response;
+                    cambioColore(numeroRandom, quadCorrente);
+                },
+                error : function(){
 
-            }
-        });
+                }
+            });
+        }
     });
 
 
+function cambioColore(numero, quadrato){
+    if (numero <= 5) {
+        quadrato.addClass("yellow");
+        console.log(numero);
+        quadrato.text(numero);
+    }else{
+        quadrato.addClass("green");
+        console.log(numero);
+        quadrato.text(numero);
 
+    }
 
-
-
-
-
+};
 
 });
